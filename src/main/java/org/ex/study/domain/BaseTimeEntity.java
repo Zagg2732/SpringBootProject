7e2c0953ce078692504b0573a626eee3560e134e
Zagg2732 <67727906+Zagg2732@users.noreply.github.com>
2021-07-09 13:13:11 +0900

    Initial commit

---
+++ src/main/java/org/ex/study/domain/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package org.ex.study.domain;
+
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    //생성시간
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    //수정시간
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
